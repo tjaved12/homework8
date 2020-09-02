db2f796cc6d1d47afe3760cab17dd87f99d86972
TJ <tahmeenaowais@yahoo.com>
2020-09-02 14:40:23 -0400

    homework fixes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function getManager(){
         }
       },
    
-   //asks role of the team member
+   //asks office Number for the Manager
     
    {
     type: "input",
@@ -73,6 +73,7 @@ if(data.team=="Y"){
             "Engineer",
         ]
     },
+    //if else depending on role
 ]).then(function (data) {
 if(data.role =="Intern"){
     getIntern()
@@ -128,7 +129,7 @@ function getEngineer(){
         }
       },
    
-   //asks role of the team member
+   //asks git ID for the team member
    {
     type: "input",
     name: "git",
@@ -164,8 +165,9 @@ else if (data.role =="Engineer"){
     getEngineer()
 }
 }) 
-engineerArray(data)
 }
+engineerArray(data)
+
 function engineerArray(data){
 const engineer = new Engineer(data.name,data.id, data.email, data.git)             
 employeeArray.push(engineer)
@@ -207,7 +209,7 @@ function getIntern(){
         }
       },
    
-   //asks role of the team member
+   //asks schoolof the team member
     
    {
     type: "input",
@@ -244,7 +246,9 @@ else{
 })}
 internArray(data)
 
+
 })}
+//push data into array
 function internArray(data){
 const intern = new Intern(data.name,data.id, data.email, data.school)             
 employeeArray.push(intern)
@@ -252,9 +256,9 @@ rendermyEmployees()
    
 }
 };
-
+//write information into html file
 function rendermyEmployees() {
- fs.writeFileSync("test.html", render(employeeArray),
+ fs.writeFileSync("team.html", render(employeeArray),
  function (err) {
 
     if (err) {
@@ -268,27 +272,4 @@ function rendermyEmployees() {
 
  getManager()
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
